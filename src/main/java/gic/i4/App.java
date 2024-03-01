b33de301553b10c608ec83f9825b52f2e111a148
Sreng <tongsreng@itc.edu.kh>
2024-03-01 14:03:41 +0700

    test archive artifact

--- src/main/java/gic/i4/App.java
+++ src/main/java/gic/i4/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "I4A are too many" );
+        System.out.println( "I4A are too many." );
+        System.out.println( "I4A archiving the artifact." );
     }
 }
