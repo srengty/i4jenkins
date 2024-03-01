422ae5b9ed051bc8d5b8051b07cbaf07e74fbf04
Sreng <tongsreng@itc.edu.kh>
2024-03-01 10:30:04 +0700

    make error commit

--- src/main/java/gic/i4/App.java
+++ src/main/java/gic/i4/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "I4 TP 1 Autobuild trigger every minute" );
+        System.out.println(Happy birthday);
     }
 }
